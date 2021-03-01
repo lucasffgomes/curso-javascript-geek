c3c1690a3bd078fd3d0a1759faa2069ba99f9710
Lucas Gomes <lucasgomes@compbyte.com.br>
2021-03-01 16:48:04 -0300

    v2.2

--- secao05/aula04.js
+++ secao05/aula04.js
@@ -1,9 +1,35 @@
 var valores = [2, 4, 6, 8, 10];
 console.log(valores);
 
+// map gera um novo array apartir do original
 var dobro = valores.map(function(valor) {
     return valor * 2;
 });
 
-console.log(dobro);
+// console.log(dobro);
 
+function dobrar(valor) {
+    return valor * 2;
+}
+
+dobro = valores.map(dobrar);
+
+// console.log(dobro);
+
+// encadeando maps para realizar multpilas transformacoes
+function somar_4(valor) {
+    return valor + 4;
+}
+
+function dividir_5(valor) {
+    return valor / 5;
+}
+
+var resultado = valores.map(dobrar).map(somar_4).map(dividir_5);
+console.log(resultado);
+
+/* 
+passo 1 - dobrar [4, 8, 12, 16, 20]
+passo 2 - soma_4 [8, 12, 16, 20, 24]
+passo 3 - dividir_5 [1.6, 2.4, 3.2, 4, 4.8]
+*/
---
+++ secao05/aula05.js
@@ -0,0 +1,7 @@
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function filtrar_pares(n) {
+    return n % 2 === 0;
+}
+
+console.log(filtrar_pares(3));  // false
