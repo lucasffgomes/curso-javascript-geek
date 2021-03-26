bcc7482e9a02e3e28cc7d8b8b695d9c76d7428f7
Lucas Gomes <lucasgomes@compbyte.com.br>
2021-03-26 10:08:50 -0300

    Update aula05.js

--- secao05/aula05.js
+++ secao05/aula05.js
@@ -4,4 +4,45 @@ function filtrar_pares(n) {
     return n % 2 === 0;
 }
 
-console.log(filtrar_pares(3));  // false
+// console.log(filtrar_pares(3));  // false
+// console.log(filtrar_pares(8));  // true
+
+function filtrar_impares(n) {
+    return n % 2 !== 0;
+}
+
+function filtrar_multiplos_5(n) {
+    return n % 5 === 0;
+}
+
+var ret = numeros.filter(filtrar_pares);  // gerar um novo array com numeros pares
+// console.log(ret);
+
+ret = numeros.filter(filtrar_impares);  // gerar um novo array com numeros impares
+// console.log(ret);
+
+ret = numeros.filter(filtrar_multiplos_5);  // gerar um novo array com numeros multiplos de 5
+// console.log(ret);
+
+var alunos = [
+    {nome: "Pedro", nota: 8.5},
+    {nome: "Maria", nota: 10},
+    {nome: "Fernanda", nota: 9.2},
+    {nome: "Ricardo", nota: 7.3},
+]
+
+function filtrar_notas_maiores_que_8(aluno) {
+    return aluno.nota > 8;
+}
+
+function filtrar_notas_maiores_que_7(aluno) {
+    return aluno.nota > 7;
+}
+
+var filtrados = alunos.filter(filtrar_notas_maiores_que_8);
+console.log(filtrados);
+console.log(filtrados.length);
+
+filtrados = alunos.filter(filtrar_notas_maiores_que_7);
+console.log(alunos.length);
+console.log(filtrados.length);
